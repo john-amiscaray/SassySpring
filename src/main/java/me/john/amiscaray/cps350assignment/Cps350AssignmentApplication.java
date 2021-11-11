cce8b5644e2792362e58a2892357675e22775a26
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-11-11 16:39:46 -0500

    init spring project. This closes #1.

---
+++ src/main/java/me/john/amiscaray/cps350assignment/Cps350AssignmentApplication.java
@@ -0,0 +1,13 @@
+package me.john.amiscaray.cps350assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cps350AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cps350AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/john/amiscaray/cps350assignment/Cps350AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.john.amiscaray.cps350assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cps350AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
